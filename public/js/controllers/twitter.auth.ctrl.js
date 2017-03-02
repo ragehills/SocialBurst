6c87e587b3f6393fdd74b8302b151c09ba7cd99f
Rage Hills <1207581brunel@gmail.com>
2017-03-02 20:14:55 +0000

    openweather api start

--- public/js/controllers/twitter.auth.ctrl.js
+++ public/js/controllers/twitter.auth.ctrl.js
@@ -2,8 +2,24 @@ angular
 	.module('social-burst')
 	.controller('twitterAuthenticationController', twitterAuthenticationController)
 
-function twitterAuthenticationController(Auth, User, $state) {
-	var provider = new firebase.auth.TwitterAuthProvider();
+var provider = new firebase.auth.TwitterAuthProvider();
 
-	
-}	
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+  // You can use these server side with your app's credentials to access the Twitter API.
+  var token = result.credential.accessToken;
+  var secret = result.credential.secret;
+  // The signed-in user info.
+  var user = result.user;
+  consol.log(user)
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
