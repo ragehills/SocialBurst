204b8dd127250afb301f32c141720af4df065266
Rage Hills <1207581brunel@gmail.com>
2017-03-02 21:47:14 +0000

    loading location weather information on the homepage

--- public/js/controllers/openWeather.ctrl.js
+++ public/js/controllers/openWeather.ctrl.js
@@ -9,6 +9,18 @@ function weathercontroller($http) {
 	$http.get("http://ip-api.com/json").success(function(data) {
 		self.lat = data.lat;
 		self.lon = data.lon;
+
+		var apiKey = 
+		var openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + self.lat + "&lon=" + self.lon + "&appid=" +apiKey;
+		
+		$http.get(openWeatherURL).success(function(data) {
+			self.description = data.weather[0].description;
+			self.speed = (2.237*data.wind.speed).toFixed(1) + " mph";
+			self.name = data.name;
+			self.temp = data.main.temp;
+			self.fTemp = (self.temp*(9/5)-459.67).toFixed(1) + " (°F)";
+			self.cTemp = (self.temp-273).toFixed(1) + " (°C)";
+		});
 	});
 };
 
